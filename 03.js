6ededdbe4be52f7db697dbf93a58aa283ddccd83
mahammadshariaralamsarker <154776315+mahammadshariaralamsarker@users.noreply.github.com>
2024-01-03 15:17:21 +0600

    sum line

--- 03.js
+++ 03.js
@@ -3,7 +3,7 @@ function addition(num1, num2) {
   var sum = 0;
   for (var i = 0; i <= arguments.length; i++) {
     const num = arguments[i] ;
-    sum = sum+i;
+  
     console.log(num);
   }
   return num1 + num2;
