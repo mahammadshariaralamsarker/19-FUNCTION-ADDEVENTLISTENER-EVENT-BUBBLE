4ccdc9a2c28f9f66bfd2ad81717e5c01f20ed750
mahammadshariaralamsarker <154776315+mahammadshariaralamsarker@users.noreply.github.com>
2024-01-03 15:20:12 +0600

    final commit

--- 03.js
+++ 03.js
@@ -2,9 +2,8 @@ function addition(num1, num2) {
 //   console.log(arguments[2]);
   var sum = 0;
   for (var i = 0; i <= arguments.length; i++) {
-    const num = arguments[i] ;
-  
     console.log(num);
+  sum = sum+i;
   }
   return num1 + num2;
 }
